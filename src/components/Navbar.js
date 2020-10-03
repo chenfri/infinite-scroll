789da086d4300a5c8808a5a11aa588f2d16dffef
unknown <chenfriedman93@gmail.com>
2020-10-03 11:39:21 +0300

    Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Navbar = () => { 
+        return (
+            
+        <nav className="navbar navbar-expand-sm navbar-dark bg-info mb-3 py-2">
+            <div className="container">
+                <a className="navbar-brand font-weight-bold" style={{fontFamily:"cursive"}}>
+                    <i class="far fa-images"></i> infinite-scroll image viewer
+                </a>
+            </div>
+        </nav>
+         
+
+        );  
+}
+
+export default Navbar;
